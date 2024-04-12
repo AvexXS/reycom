eb2e08d94ca5f63533f81867569fa191e46eac03
Avex Boullevard <159640468+AvexXS@users.noreply.github.com>
2024-04-12 16:07:54 +0530

    Update HomeCard.tsx

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -26,8 +26,8 @@ const HomeCard = ({ className, img, title, description, handleClick }: HomeCardP
       </div>
       
       <div className="flex flex-col gap-2">
-        <h1 className="text-2xl font-bold">{title}</h1>
-        <p className="text-lg font-normal">{description}</p>
+        <h1 className="text-2xl font-bold text-black">{title}</h1>
+        <p className="text-lg font-normal text-black">{description}</p>
       </div>
     </section>
   );
